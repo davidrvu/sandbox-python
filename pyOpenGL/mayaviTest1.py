2c6b808791d5765ca103080c690f2f1d31c3f9e5
davidinj <david.valenzuela.u@gmail.com>
2017-02-06 18:02:49 -0300

    mayavi install

--- pyOpenGL/mayaviTest1.py
+++ pyOpenGL/mayaviTest1.py
@@ -2,11 +2,12 @@
 # PASO 2: Microsoft Visual C++ 9.0 is required. Get it from http://aka.ms/vcpython27
 # 			(INSTALAR Microsoft Visual C++ Compiler for Python 2.7)
 # PASO 3: Instalar VTK para WINDOWS, desde http://www.vtk.org/download/
-# PASO 4: pip install pyvtk
+# PASO 4: conda install -c clg_boar vtk=7.0.0
 # PASO 5: pip install mayavi
+# PASO 6: conda install -c anaconda wxpython=3.0.0.0
 
-
-import enthought.mayavi.mlab as mylab
+#import enthought.mayavi.mlab as mylab
+import mayavi.mlab as mylab
 import numpy as np
 x, y, z, value = np.random.random((4, 40))
 mylab.points3d(x, y, z, value)
