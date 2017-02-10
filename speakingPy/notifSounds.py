c3e2632508aebc38301206194816141abe12fc25
davidrvu <david.valenzuela.u@gmail.com>
2017-02-09 23:31:53 -0300

    notifSounds.py

---
+++ speakingPy/notifSounds.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#!/usr/bin/env python
+  
+import winsound
+
+def sound_error():
+    winsound.Beep(450,100) # (frecuencia[Hz], duration[ms])
+    winsound.Beep(300,100) # (frecuencia[Hz], duration[ms])
+    winsound.Beep(250,100) # (frecuencia[Hz], duration[ms])
+    winsound.Beep(450,100) # (frecuencia[Hz], duration[ms])
+    winsound.Beep(300,100) # (frecuencia[Hz], duration[ms])
+    winsound.Beep(250,100) # (frecuencia[Hz], duration[ms])
