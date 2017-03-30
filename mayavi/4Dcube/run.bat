3c8535ab5362a8c0ab07427b379c7badc933abf4
davidinj <david.valenzuela.u@gmail.com>
2017-03-30 16:39:09 -0300

    run 4Dcube

---
+++ mayavi/4Dcube/run.bat
@@ -0,0 +1,10 @@
+::~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+:: RUN 4Dcube
+::~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+@echo off
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo RUN 4Dcube
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+echo RUN example3
+CALL timecmd python example3.py
