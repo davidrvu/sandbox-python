7b431fa57bb64ae96d10634d960892aa5885a1ba
David Valenzuela Urrutia <david.valenzuela.u@gmail.com>
2017-12-27 14:25:41 -0300

    plotly_grouped_bar con parametros opcionales

--- plotly/plotly_grouped_bar.py
+++ plotly/plotly_grouped_bar.py
@@ -1,14 +1,28 @@
+# DAVIDRVU - 2017
+
 from plotly.graph_objs import Scatter, Figure, Layout, Scattergl, Bar
 from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 
-def plotly_grouped_bar(allTrace, filename, title_in):
+def plotly_grouped_bar(allTrace, filename, title_in, **kwargs):
     print("\n----> START plotly_grouped_bar")
+
+    margin_left = kwargs.get('margin_left', 100)         # default value
+    xaxis_title = kwargs.get('xaxis_title', "Frequency") # default value 
+    yaxis_title = kwargs.get('yaxis_title', "Type")      # default value 
+
     layout = {
                 'barmode': 'group',
                 'showlegend': True,
                 'title': title_in,
                 'yaxis': {
+                    'title': yaxis_title,
                     'autorange': 'reversed',
+                },
+                'xaxis': {
+                    'title': xaxis_title,
+                },
+                'margin': {
+                    'l': margin_left,
                 }
             }
     fig = dict(data = allTrace, layout = layout)
