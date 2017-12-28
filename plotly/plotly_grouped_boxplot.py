57dbd560173156db455b5a46a8fd94f862ee45ff
David Valenzuela Urrutia <david.valenzuela.u@gmail.com>
2017-12-28 09:22:59 -0300

    plotly grouped boxplot

---
+++ plotly/plotly_grouped_boxplot.py
@@ -0,0 +1,34 @@
+# DAVIDRVU - 2017
+
+from plotly.graph_objs import Scatter, Figure, Layout, Scattergl, Bar
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+
+def plotly_grouped_boxplot(allTrace, filename, title_in, **kwargs):
+    print("\n----> START plotly_grouped_boxplot")
+
+    margin_left = kwargs.get('margin_left', 100)         # default value
+    xaxis_title = kwargs.get('xaxis_title', "Frequency") # default value 
+    yaxis_title = kwargs.get('yaxis_title', "Type")      # default value 
+
+    layout = {
+                'boxmode': 'group',
+                'showlegend': True,
+                'title': title_in,
+                'yaxis': {
+                    'title': yaxis_title,
+                    #'autorange': 'reversed',
+                    'zeroline':False,
+                },
+                'xaxis': {
+                    'title': xaxis_title,
+                },
+                'margin': {
+                    'l': margin_left,
+                }
+            }
+
+    fig = dict(data = allTrace, layout = layout)
+    print("PLOTLY_GROUPED_BAR filename = " + filename)
+    plot(fig, filename=filename)
+
+    print("----> END plotly_grouped_boxplot\n") 
