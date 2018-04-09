8bb5551650681bce235f092c65cc5c34f9cdbe25
davidrvu <david.valenzuela.u@gmail.com>
2018-04-09 12:51:55 -0300

    plotly streaming online

--- plotly/streaming/sender_data.py
+++ plotly/streaming/sender_data.py
@@ -1,26 +1,69 @@
 # DAVIDRVU 2018
 
-import plotly
-from plotly.graph_objs import *
-from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+# SOURCE: https://stackoverflow.com/questions/28161244/how-to-create-two-y-axes-streaming-plotly
+
+from plotly.graph_objs import Scatter, Layout, Figure, Data, Stream, YAxis
 import datetime
+import numpy as np
+import plotly.plotly as py # plotly library
+import sys
 import time
 
 
 def main():
     print("SENDER DATA")
 
-    stream_token = 'AAABBB'
+    stream_token_temperature = 'aghuvyozmm'
+    stream_token_humidity    = '5mfgh2l9jl'
 
-    stream = plotly.plotly.Stream(stream_token)
-    stream.open()
+    stream_temperature = py.Stream(stream_token_temperature)
+    stream_temperature.open()
 
-    y_data = 0
-    while True:
-        
-        stream.write({'x': datetime.datetime.now(), 'y': y_data})
-        y_data = y_data + 1
-        time.sleep(0.1) # delay between stream posts
+    stream_humidity = py.Stream(stream_token_humidity)
+    stream_humidity.open()
 
+    print("Starting main loop ...")
+    x_count   = 0 
+    freq_temp = 10
+    freq_hum  = 5
+    Fs        = 100
+
+    temp_min = -5
+    temp_max = 35
+
+    humidity_min = 10
+    humidity_max = 30
+
+    temp_amp = (temp_max - temp_min)/2.0
+    humi_amp = (humidity_max - humidity_min)/2.0
+
+    temp_mean = np.mean([temp_max, temp_min])
+    humi_mean = np.mean([humidity_max, humidity_min])
+
+    print("temp_amp  = " + str(temp_amp))
+    print("humi_amp  = " + str(humi_amp))
+
+    print("temp_mean = " + str(temp_mean))
+    print("humi_mean = " + str(humi_mean))
+
+    while(True):
+
+        temp     = round((temp_amp * np.sin(2 * np.pi * freq_temp * x_count / Fs) + 0        ) + temp_mean , 3)
+        humidity = round((humi_amp * np.sin(2 * np.pi * freq_hum  * x_count / Fs) + (np.pi/2)) + humi_mean , 3) 
+
+        time_now = datetime.datetime.now()
+
+        print(str(time_now) + " | Temp = " + str(temp) + " | humidity = " + str(humidity))
+
+        stream_temperature.write({'x': time_now, 'y': temp })
+        stream_humidity.write({'x': time_now, 'y': humidity })
+
+        x_count = x_count + 1
+
+        time.sleep(2)
+
+    stream_temperature.close()
+    stream_humidity.close()
+    
 if __name__ == "__main__":
     main()
--- plotly/streaming/sender_receiver_plot_example.py
+++ plotly/streaming/sender_receiver_plot_example.py
@@ -10,8 +10,6 @@ import sys
 import time
 
 def main():
-    print("RECEIVER AND PLOT")
-
     # Plot.ly credentials and stream tokens
     username                 = 'davidrvu'
     api_key                  = 'hOQ0feXyuUxZ0WtrhUgt'
@@ -60,6 +58,56 @@ def main():
     fig = Figure(data=data, layout=layout)
 
     print(py.plot(fig, filename='Plotly streaming test'))
+
+    stream_temperature = py.Stream(stream_token_temperature)
+    stream_temperature.open()
+
+    stream_humidity = py.Stream(stream_token_humidity)
+    stream_humidity.open()
+
+    print("Starting main loop ...")
+    x_count   = 0 
+    freq_temp = 10
+    freq_hum  = 5
+    Fs        = 100
+
+    temp_min = -5
+    temp_max = 35
+
+    humidity_min = 10
+    humidity_max = 30
+
+    temp_amp = (temp_max - temp_min)/2.0
+    humi_amp = (humidity_max - humidity_min)/2.0
+
+    temp_mean = np.mean([temp_max, temp_min])
+    humi_mean = np.mean([humidity_max, humidity_min])
+
+    print("temp_amp  = " + str(temp_amp))
+    print("humi_amp  = " + str(humi_amp))
+
+    print("temp_mean = " + str(temp_mean))
+    print("humi_mean = " + str(humi_mean))
+
+    while(True):
+
+        temp     = round((temp_amp * np.sin(2 * np.pi * freq_temp * x_count / Fs) + 0        ) + temp_mean , 3)
+        humidity = round((humi_amp * np.sin(2 * np.pi * freq_hum  * x_count / Fs) + (np.pi/2)) + humi_mean , 3) 
+
+        time_now = datetime.datetime.now()
+
+        print(str(time_now) + " | Temp = " + str(temp) + " | humidity = " + str(humidity))
+
+        stream_temperature.write({'x': time_now, 'y': temp })
+        stream_humidity.write({'x': time_now, 'y': humidity })
+
+        x_count = x_count + 1
+
+        time.sleep(2)
+
+    stream_temperature.close()
+    stream_humidity.close()
+
 
 if __name__ == "__main__":
     main()
