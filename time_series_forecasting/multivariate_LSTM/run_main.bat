6ade7cee678079a54cdfb40557707fabcbbb1a8e
David R Valenzuela U <drvalenzuela@Falabella.cl>
2018-06-13 11:48:23 -0400

    multivariate LSTM run file

---
+++ time_series_forecasting/multivariate_LSTM/run_main.bat
@@ -0,0 +1,7 @@
+:: DAVIDRVU 2018
+@echo off
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo  RUN Multivariate Time Series Forecasting with LSTMs in Keras
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+CALL timecmd python main.py
