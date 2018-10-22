be16ca7e45047e9b0dd4a779bce83b33c4b7b769
davidinj <david.valenzuela.u@gmail.com>
2018-10-21 22:32:46 -0300

    Selenium test -> successful!

--- Robotic_Process_Automation/Selenium/test1.py
+++ Robotic_Process_Automation/Selenium/test1.py
@@ -1,25 +1,22 @@
 # DAVIDRVU - 2018
 
-# INSTALAR:
-# pip install selenium
-# pip install chromedriver
-# pip install chromedriver-binary
+# 1) INSTALAR:
+#      pip install selenium
+# 2) DESCARGAR CHROMEDRIVER DE:
+#      https://sites.google.com/a/chromium.org/chromedriver/downloads
+#      VERSION: chromedriver_win32.zip
+# 3) Poner en PATH el archivo chromedriver.exe
+# 4) EJECUTAR ESTE SCRIPT!
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 URL_str = "http://www.yahoo.com"
-
-#ex_path = "/usr/local/bin/chromedriver" # OJO REVISAR
-ex_path = "C:/Anaconda3/Lib/site-packages/chromedriver"
-browser = webdriver.Chrome(executable_path=ex_path)
+browser = webdriver.Chrome()
 browser.get(URL_str)
-
-
-
-#assert 'Yahoo' in browser.title
 
 elem = browser.find_element_by_name('p')  # Find the search box
 elem.send_keys('seleniumhq' + Keys.RETURN)
 
 #browser.quit()
+print("DONE!")
